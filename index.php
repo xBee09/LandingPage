815c0bae4d169828bee7ac1574e1cdde5ba83754
Faris Anugrah <129975202+xBee09@users.noreply.github.com>
2024-06-21 19:01:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,274 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>KulinerSIa</title>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar">
+            <ul class="menu-items">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#food-menu">Product</a></li>
+                <li><a href="#best-seller">Best Seller</a></li>
+                <li><a href="#buku-tamu">Guest Book</a></li>
+                <h1 class="logo">KulinerSIa.dinus</h1>
+            </ul>
+        </div>
+    </nav>
+    <!-- banner -->
+    <section class="banner-area" id="banner">
+        <div class="banner-container">
+            <h1 class="main-title" id="home">WELCOME TO KULINER PADANG</h1>
+            <a href="#food-menu" class="btn btn-primary">Product</a>
+        </div>
+    </section>
+    <!-- about -->
+    <section id="about">
+        <div class="about-wrapper container">
+            <div class="about-text">
+                <p class="small">About Us</p>
+                <h2>KulinerSIa.dinus </h2>
+                <p>
+                    Tradisi dalam menjaga cita rasa yang otentik, tempat yang modern dan nyaman, serta pelayanan yang ramah, adalah semua tentang KulinerSIa Restaurant.
+
+                    KulinerSIa telah berdiri sejak tahun 2023, dan terus mengembangkan diri untuk memastikan cita rasa dan kualitas terbaik. Kami melakukan banyak perjalanan untuk mempelajari berbagai hal, termasuk mencari bahan baku pilihan. Dan kami bangga menjadi salah satu Restoran Padang terbaik.
+                </p>
+            </div>
+            <div class="about-img">
+                <img src="asset/about.png" alt="">
+            </div>
+        </div>
+    </section>
+    <!-- product -->
+    </section>
+    <section id="food-menu">
+        <h2 class="food-menu-heading">Product</h2>
+        <div class="food-menu-container container">
+            <div class="food-menu-item">
+                <div class="food-img">
+                    <img src="asset/rendang.jpg" alt="" />
+                </div>
+                <div class="food-description">
+                    <h2 class="food-titile">Rendang</h2>
+                    <p>
+                        Rendang merupakan salah satu makanan khas Padang yang terbuat dari daging sapi yang dimasak menggunakan aneka bumbu, maka dari itu kelezatan makanan ini tidak perlu diragukan lagi.
+                    </p>
+                    <p class="food-price">Harga: Rp. 12.000,00</p>
+                </div>
+            </div>
+
+            <div class="food-menu-item">
+                <div class="food-img">
+                    <img src="asset/ayam pop.webp" alt="error" />
+                </div>
+                <div class="food-description">
+                    <h2 class="food-titile">Ayam Pop</h2>
+                    <p>
+                        Ayam pop menjadi salah satu sajian populer jika sedang berkunjung ke rumah makan padang. Ayam pop merupakan kategori ayam goreng yang memiliki cita rasa yang gurih dan nikmat.
+                    </p>
+                    <p class="food-price">Harga: Rp. 8.000,00</p>
+                </div>
+            </div>
+            <div class="food-menu-item">
+                <div class="food-img">
+                    <img src="asset/dendeng balado.jpeg" alt="" />
+                </div>
+                <div class="food-description">
+                    <h2 class="food-titile">Dendeng Balado</h2>
+                    <p>
+                        Dendeng balado biasanya digoreng setengah garing kemudian dipadukan dengan bumbu cabai yang berlimpah sehingga membuat rasanya pedas dan nikmat
+                    </p>
+                    <p class="food-price">Harga: Rp. 7.000,00</p>
+                </div>
+            </div>
+            <div class="food-menu-item">
+                <div class="food-img">
+                    <img src="asset/ikan asam.webp" alt="" />
+                </div>
+                <div class="food-description">
+                    <h2 class="food-titile">Ikan Asam Padieh</h2>
+                    <p>
+                        Ikan asam padieh merupakan salah satu kuliner yang cukup populer di rumah makan Padang. Makanan ini merupakan jenis gulai ikan yang memiliki kuah asam pedas.
+                    </p>
+                    <p class="food-price">Harga: Rp. 10.000,00</p>
+                </div>
+            </div>
+            <div class="food-menu-item">
+                <div class="food-img">
+                    <img src="asset/soto padang.webp" alt="" />
+                </div>
+                <div class="food-description">
+                    <h2 class="food-titile">Soto Padang</h2>
+                    <p>
+                        Soto Padang disajikan dengan kuah kaldu yang gurih dan berempah dengan isian soun, dendeng sapi atau daging sapi goreng, kentang goreng atau perkedel, daun bawang, bawang goreng, dan kerupuk merah
+                    </p>
+                    <p class="food-price">Harga: Rp. 9.500,00</p>
+                </div>
+            </div>
+            <div class="food-menu-item">
+                <div class="food-img">
+                    <img src="asset/sate padang.jpg" alt="" />
+                </div>
+                <div class="food-description" >
+                    <h2 class="food-titile">Sate Padang</h2>
+                    <p>
+                    Sate Padang berbeda dari sate yang ada di daerah lain, sate padang disajikan dengan saus berempah yang dimasak dengan tepung beras hingga kental
+                    </p>
+                    <p class="food-price">Harga: Rp. 15.000,00</p>
+                </div>
+            </div>
+        </div>
+        <!-- button selengkapnya -->
+        <div class="btn-web">
+        <button onclick="window.location.href='http://surl.li/pclyy'">Baca Selengkapnya</button> 
+        </div>
+    </div>
+  </div>
+        <!-- best seller -->
+    </section>
+    <section id="best-seller">
+        <h2 class="best-seller-title">Best Seller</h2>
+        <div class="best-seller-container container">
+            <div class="best-seller-box">
+               
+                <div class="product-detail">
+                    <div class="product-photo">
+                        <img src="asset/rendang.jpg" alt=""/>
+                        <p class="product-name">Rendang</p>
+                    </div>
+                </div>
+                <div class="bintang">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                </div>
+                </p>
+            </div>
+            <div class="best-seller-box">
+                <div class="product-detail">
+                    <div class="product-photo">
+                        <img src="asset/sate padang.jpg" alt="" />
+                        <p class="product-name">Sate Padang</p>
+                    </div>
+                </div>
+                <p class="product-text">
+                <div class="bintang">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+                </div>
+                </p>
+            </div>
+            <div class="best-seller-box">
+                <div class="product-detail">
+                    <div class="product-photo">
+                        <img src="asset/dendeng balado.jpeg" alt="" />
+                        <p class="product-name">Dendeng Balado</p>
+                    </div>
+                </div>
+                <p class="product-text">
+                <div class="bintang">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                </p>
+            </div>
+        </div>
+    </section>
+    <!-- buku tamu -->
+    <section id="buku-tamu">
+        <div class="buku-tamu-container container">
+            <div class="buku-tamu-img">
+                <img src="asset/padang.webp" alt="" />
+            </div>
+
+            <div class="form-container">
+            <h2>Buku Tamu</h2>
+            <form method="POST" action="<?php echo $_SERVER ['PHP_SELF'];?>">
+        <div>
+            <label>Nama</label>
+            <input type="text" name="fnama">
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="email" name="femail">
+        </div>
+        <div>
+            <label>Pesan</label>
+            <input type="pesan" name="fpesan">
+        </div>
+        <div>
+            <input type="submit" name="Submit">
+        </div>
+    </form>
+
+        <div>
+            <?php
+                if(isset($_POST['fnama'])){
+                    $nama = $_POST['fnama'];
+                    $email = $_POST['femail'];
+                    $pesan = $_POST['fpesan'];
+                    echo "Nama: $nama, email: $email, pesan: $pesan";
+                }
+                ?>
+        </div>
+        </div>
+    </section>
+
+    <div class="get">
+            <h2>Buku Tamu</h2>
+            <form method="GET" action="<?php echo $_SERVER ['PHP_SELF'];?>">
+            <label>Pilih makanan yang anda suka</label>
+            <select name="kesukaan" id="favorit">
+                <option value="">Pilih</option>
+                <option value="Rendang">Rendang</option>
+                <option value="Ayam pop">Ayam pop</option>
+                <option value="Dendeng balado">Dendeng Balado</option>
+                <option value="Ikan asam padieh">Ikan Asam Padieh</option>
+                <option value="soto padang">Soto padang</option>
+                <option value="Sate padang">Sate Padang</option>
+            </select>
+            <button type="submit" value="kirim"> Kirim</button>
+    </form>
+            </div>  
+        <div>
+            <?php
+                if(isset($_GET['kesukaan'])){
+                    $favorit = $_GET['kesukaan'];
+                ?>
+                    <div class="output">
+                        <p>makanan yang disukai pelanggan : <?php echo $favorit; ?></p>
+                    </div>
+                    <?php
+                }
+                ?>
+        </div>
+        </div>
+
+    <!-- footer -->
+
+    <footer id="footer">
+        <h2>KulinerSIa.dinus &copy; all rights reserved</h2>
+    </footer>
+    
+    <!-- button scroll -->
+
+    <button id="scrollToTopBtn">Top</button>
+    <script src="script.js"></script>
+    
+</html>
+
+</body>
+
+</html>
